5001a442b8b5cfcc410afa5f80c58b188a7e41ff
Donovan <dvnstevenb@gmail.com>
2020-07-28 21:33:19 -0500

    pending

--- Secado/main.py
+++ Secado/main.py
@@ -125,11 +125,11 @@ class Thread(QThread):
         self.data = data
 
     def run(self):
-        schedule.every(1).seconds.do(self.data.env)
-        schedule.every(2).seconds.do(self.data.env1)
-        schedule.every(3).seconds.do(self.data.env2)
-        schedule.every(4).seconds.do(self.data.env3)
-        schedule.every(5).seconds.do(self.data.client.verifyPending)
+        schedule.every(3).seconds.do(self.data.env)
+        schedule.every(4).seconds.do(self.data.env1)
+        schedule.every(5).seconds.do(self.data.env2)
+        schedule.every(6).seconds.do(self.data.env3)
+        schedule.every(7).seconds.do(self.data.client.verifyPending)
         schedule.every(10).seconds.do(self.data.save)
 
         while self.threadactive:
