21fdcce1f408280ebbf7bfe66017cad84fc4f9fa
Donovan <dvnstevenb@gmail.com>
2020-08-22 23:22:54 -0500

    Estaciones listas

--- Secado/src/widgets/widgets.py
+++ Secado/src/widgets/widgets.py
@@ -171,7 +171,6 @@ class MenuBar:
 
         # Estado de la aplicacion
         self.state = QLabel()
-        self.state.setStyleSheet('font-weight:bold')
         hbox = QHBoxLayout(self.menuBar)
         hbox.addWidget(self.state)
         hbox.setAlignment(Qt.AlignCenter)
