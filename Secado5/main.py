5f8612c2a10c1eccd4bfdfcfdcd8b3513512d0b0
DonovanSB <dvnstevenb@gmail.com>
2021-04-14 11:48:50 -0500

    Estacion secado 5 zonas

---
+++ Secado5/main.py
@@ -0,0 +1,165 @@
+import sys
+import os
+import time
+import schedule
+from PyQt5.QtWidgets import QApplication, QMessageBox, QMainWindow, QDesktopWidget, QWidget, QGridLayout
+from PyQt5.QtCore import QThread, QCoreApplication, QProcess
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+route = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(route + "/src/widgets")
+sys.path.append(route + "/src/providers")
+import widgets
+import provider
+
+rutaProviders = route + "/src/providers"
+
+class Estacion(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        #--- Variables Ventana------
+        self.sizeWindow = QDesktopWidget().availableGeometry()
+        #print(sizeWindow)
+        self.xWindow = self.sizeWindow.x()
+        self.yWindow = self.sizeWindow.y()
+        self.width = 900
+        self.height = 550
+
+        #----Inicializacion Ventana---
+        self.setWindowTitle("Estación Secado")
+        self.setGeometry(self.yWindow,self.xWindow,self.width,self.height)
+        self.setStyleSheet("QMainWindow {background: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 #9e9e9e, stop:1 #707070)}")
+        self.centerWindow()
+        # signals
+        self.signals = provider.Signals()
+        self.signals.signalUpdateGraph.connect(self.updateGraph)
+        self.signals.signalAlert.connect(self.alerts)
+        self.signals.signalMessages.connect(self.messages)
+        self.signals.signalUpdatePrefs.connect(self.restartApp)
+
+        #---Crear Widgets---
+        self.createWidgets()
+
+        #--- Inicio de Instacias de providers
+        self.plot = provider.Plotter(self.graph.figure,self.graph.ax1)
+        self.data = provider.Data(self.loading,
+                                  self.environment.tempValue,
+                                  self.environment.humValue,
+                                  self.zone1.tempValue,
+                                  self.zone1.humValue,
+                                  self.zone2.tempValue,
+                                  self.zone2.humValue,
+                                  self.zone3.tempValue,
+                                  self.zone3.humValue,
+                                  self.zone4.tempValue,
+                                  self.zone4.humValue,
+                                  self.zone5.tempValue,
+                                  self.zone5.humValue,
+                                  self.humGrain.text)
+        self.prefs = provider.LocalStorage(route=rutaProviders, name = 'prefs')
+
+        #----Iniciar subproceso---
+        self.initTask = True
+
+        self.thread = Thread(self.data)
+        self.thread.start()
+
+        #-- Mostrar Ventana---
+        self.show()
+        self.setFocus()
+    def restartApp(self):
+        QCoreApplication.quit()
+        QProcess.startDetached(sys.executable, sys.argv)
+
+    def closeEvent(self, event):
+        reply = QMessageBox.question(None,' ',"¿Realmente desea cerrar la aplicación?", 
+                QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+        if reply == QMessageBox.Yes:
+            self.thread.stop()
+            self.data.threadForever.stop()
+            self.data.sqlite.con.close()
+            self.data.client.sqlite.con.close()
+            event.accept()
+        else:
+            event.ignore()
+
+    def alerts(self, message):
+        QMessageBox.critical(None,'Alerta',message, QMessageBox.Ok)
+
+    def messages(self, message):
+        self.menuBar.state.setText(message)
+
+    def centerWindow(self):
+        sizeScreen = QDesktopWidget().availableGeometry().center()
+        sizeWindow = self.geometry()
+        self.move(sizeScreen.x()-sizeWindow.width()/2,sizeScreen.y() - sizeWindow.height()/2)
+
+    def createWidgets(self):
+        # Datos Actuales
+        self.environment = widgets.EnvironmentWidget("Ambiente","18 °C", "50 %")
+        self.zone1 = widgets.EnvironmentWidget("Zona 1","15 °C", "50 %")
+        self.zone2 = widgets.EnvironmentWidget("Zona 2","17 °C", "50 %")
+        self.zone3 = widgets.EnvironmentWidget("Zona 3","17 °C", "50 %")
+        self.zone4 = widgets.EnvironmentWidget("Zona 4","17 °C", "50 %")
+        self.zone5 = widgets.EnvironmentWidget("Zona 5","17 °C", "50 %")
+        self.humGrain = widgets.RoundedContainerInput("Humedad Grano","humedadpurpleSmall.svg","50 %")
+
+        # Menu de visuzalización
+        self.menuVis = widgets.MenuVisualizacion()
+        self.menuVis.listaVariables.currentIndexChanged.connect(self.signals.signalUpdateGraph.emit)
+        # Grafica
+        self.graph = widgets.Graph()
+        self.toolbarFig = NavigationToolbar(self.graph.figure, self)
+        self.toolbarFig.setStyleSheet("background:rgba(45,208,179,0); color:white; font:20; font-weight: bold")
+
+        # Menu Bar
+        self.menuBar = widgets.MenuBar(self.menuBar())
+
+        widgetGrid = QWidget()
+        gridLayout = QGridLayout(widgetGrid)
+        gridLayout.addWidget(self.environment,0,0)
+        gridLayout.addWidget(self.zone1,0,1)
+        gridLayout.addWidget(self.zone2,0,2)
+        gridLayout.addWidget(self.zone3,0,3)
+        gridLayout.addWidget(self.zone4,0,4)
+        gridLayout.addWidget(self.zone5,0,5)
+        gridLayout.addWidget(self.humGrain,1,0)
+        gridLayout.addWidget(self.menuVis,1,5)
+        gridLayout.addWidget(self.graph,1,1,1,4)
+        gridLayout.addWidget(self.toolbarFig,2,0,1,6)
+        self.setCentralWidget(widgetGrid)
+
+        self.loading = widgets.Loading(self)
+
+    def updateGraph(self):
+        # Graficar
+        indexActual = self.menuVis.listaVariables.currentIndex()
+        variableActual = self.menuVis.listaVariables.currentText()
+        self.plot.plot(self.data.getTime(indexActual), self.data.getData(indexActual),variableActual, indexActual)
+
+class Thread(QThread):
+
+    def __init__(self,data):
+        super().__init__()
+        self.threadactive = True
+        self.data = data
+        self.prefs = provider.LocalStorage(route=rutaProviders, name = 'prefs')
+        self.samplingTimes = self.data.verifySamplingTimes()
+
+    def run(self):
+        schedule.every(int(self.samplingTimes["env"])).seconds.do(self.data.env)
+        schedule.every(int(self.samplingTimes["env1"])).seconds.do(self.data.env1)
+        schedule.every(int(self.samplingTimes["env2"])).seconds.do(self.data.env2)
+        schedule.every(int(self.samplingTimes["env3"])).seconds.do(self.data.env3)
+        schedule.every(int(self.samplingTimes["env4"])).seconds.do(self.data.env4)
+        schedule.every(int(self.samplingTimes["env5"])).seconds.do(self.data.env5)
+
+        while self.threadactive:
+            schedule.run_pending()
+            time.sleep(0.1)
+    def stop(self):
+        self.threadactive = False
+
+if __name__ == '__main__':
+    App = QApplication(sys.argv)
+    window = Estacion()
+    sys.exit(App.exec_())
---
+++ Secado5/src/widgets/widgets.py
@@ -0,0 +1,238 @@
+import sys
+import os
+from PyQt5.QtWidgets import QFrame, QPushButton, QLabel, QGridLayout, QVBoxLayout, QHBoxLayout, QComboBox, QAction, QGraphicsDropShadowEffect
+from PyQt5.QtCore import Qt, QSize
+from PyQt5.QtGui import QPixmap, QColor, QIcon
+import matplotlib.dates as dates
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+from waitingspinnerwidget import QtWaitingSpinner
+parent = os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir, os.pardir)
+route = os.path.abspath(parent)
+sys.path.append(route + "/src/windows")
+import keyboard
+import login
+
+rutaAssets = route + "/assets/"
+
+class Shadow(QGraphicsDropShadowEffect):
+    def __init__(self):
+        super().__init__()
+        self.setBlurRadius(5)
+        self.setXOffset(0)
+        self.setYOffset(3)
+        self.setColor(QColor(0,0,0,100))
+
+class RoundedContainerInput(QFrame):
+    def __init__(self,title,iconName,initialValue):
+        super().__init__()
+        # Estilo del Frame
+        self.name = title
+        # Estilo del Frame
+        self.setStyleSheet("background-color: white; border-radius:10px; margin:0px 0px 0px 0px")
+        self.setFixedHeight(200)
+        shadow = Shadow()
+        self.setGraphicsEffect(shadow)
+
+        #--- Titulo----
+        labelTitle = QLabel(title)
+        labelTitle.setStyleSheet("""background: #0d47a1;
+                                    color: white;
+                                    font: 20px;
+                                    font-weight:bold;
+                                    border-top-left-radius: 10px;
+                                    border-top-right-radius: 10px;
+                                    border-bottom-right-radius: 0px;
+                                    border-bottom-left-radius: 0px;
+                                    margin:0px 0px 0px 0px""")
+        labelTitle.setAlignment(Qt.AlignCenter)
+
+        #---Icono---
+        icon = QLabel(self)
+        pixmapIcon = QPixmap(rutaAssets + iconName)
+        icon.setPixmap(pixmapIcon)
+        icon.setStyleSheet("background: rgba(0,0,0,0); margin: 0px 0px 0px 0px")
+        icon.setAlignment(Qt.AlignCenter)
+        icon.resize(pixmapIcon.width(),pixmapIcon.height())
+
+        self.text = QLabel(initialValue)
+        self.text.setAlignment(Qt.AlignCenter)
+        self.text.setStyleSheet("background: rgba(0,0,0,0);color: black; font: 20px; margin: 0px 0px 0px 0px")
+
+        self.styleBottoms = """QPushButton {background-color:#0d47a1;
+                                            color: white;
+                                            border-radius: 20px;
+                                            font: 15px bold;
+                                            margin: 0px 5px 5px 5px;
+                                            padding: 10px;}
+                            QPushButton:hover{background-color: #5472d3}
+                            QPushButton:pressed {background-color: #002171}"""
+        buttonIngresar = QPushButton()
+        buttonIngresar.setIcon(QIcon(rutaAssets + "dialpad.svg"))
+        buttonIngresar.setIconSize(QSize(30,30))
+        buttonIngresar.setStyleSheet(self.styleBottoms)
+        buttonIngresar.clicked.connect(self.loadValue)
+
+        grid = QGridLayout(self)
+        grid.addWidget(labelTitle,0,0,1,3)
+        grid.addWidget(icon,1,0,1,3)
+        grid.addWidget(self.text,2,0,1,3)
+        grid.addWidget(buttonIngresar,3,1)
+        grid.setContentsMargins(0,0,0,0)
+
+    def loadValue(self):
+        self.keyboard = keyboard.Keyboard()
+
+class EnvironmentWidget(QFrame):
+    def __init__(self,title,initialValue1,initialValue2):
+        super().__init__()
+        # Estilo del Frame
+        self.setStyleSheet("background-color: white; border-radius:10px; margin:0px 0px 0px 0px")
+        self.setFixedHeight(200)
+        self.setGraphicsEffect(Shadow())
+        #--- Titulo----
+        labelTitle = QLabel(title)
+        labelTitle.setStyleSheet("""background: #0d47a1;
+                                    color: white;
+                                    font: 20px;
+                                    font-weight:bold;
+                                    border-top-left-radius: 10px;
+                                    border-top-right-radius: 10px;
+                                    border-bottom-right-radius: 0px;
+                                    border-bottom-left-radius: 0px;
+                                    margin:0px 0px 0px 0px""")
+        labelTitle.setAlignment(Qt.AlignCenter)
+
+        #---Icono---
+        icon = QLabel(self)
+        if title =="Ambiente":
+            pixmapIcon = QPixmap(rutaAssets + "temperaturasmall.svg")
+        else:
+            pixmapIcon = QPixmap(rutaAssets + "temperaturayellowsmall.svg")
+        icon.setPixmap(pixmapIcon)
+        icon.setStyleSheet("background: rgba(0,0,0,0); margin: 0px 0px 0px 0px")
+        icon.setAlignment(Qt.AlignCenter)
+        icon.resize(pixmapIcon.width(),pixmapIcon.height())
+
+        #---Icono---
+        icon2 = QLabel(self)
+        pixmapIcon2 = QPixmap(rutaAssets + "humedadsmall.svg")
+        icon2.setPixmap(pixmapIcon2)
+        icon2.setStyleSheet("background: rgba(0,0,0,0); margin: 0px 0px 0px 0px")
+        icon2.setAlignment(Qt.AlignCenter)
+        icon2.resize(pixmapIcon2.width(),pixmapIcon2.height())
+
+        #--- Labels--
+        labelTemperature = QLabel("Temperatura")
+        labelTemperature.setAlignment(Qt.AlignCenter)
+        labelTemperature.setStyleSheet("background: rgba(0,0,0,0);color: black; font: 16px; font-weight:bold; margin: 10px 5px 0px 0px")
+
+        self.tempValue = QLabel(initialValue1)
+        self.tempValue.setAlignment(Qt.AlignCenter)
+        self.tempValue.setStyleSheet("background: rgba(0,0,0,0);color: black; font: 20px; margin: 0px 0px 10px 0px")
+
+        labelHumidity = QLabel("Humedad")
+        labelHumidity.setAlignment(Qt.AlignCenter)
+        labelHumidity.setStyleSheet("background: rgba(0,0,0,0);color: black; font: 16px; font-weight:bold; margin: 10px 5px 0px 0px")
+
+        self.humValue = QLabel(initialValue2)
+        self.humValue.setAlignment(Qt.AlignCenter)
+        self.humValue.setStyleSheet("background: rgba(0,0,0,0);color: black; font: 20px; margin: 0px 0px 10px 0px")
+
+        grid = QGridLayout(self)
+        grid.addWidget(labelTitle,0,0,1,2)
+        grid.addWidget(icon,1,0,2,1)
+        grid.addWidget(labelTemperature,1,1)
+        grid.addWidget(self.tempValue,2,1)
+        grid.addWidget(icon2,3,0,2,1)
+        grid.addWidget(labelHumidity,3,1)
+        grid.addWidget(self.humValue,4,1)
+        grid.setContentsMargins(0,0,0,0)
+
+
+
+class MenuVisualizacion(QFrame):
+    def __init__(self):
+        super().__init__()
+         # Estilo del Frame
+        self.setStyleSheet("background-color: white; border-radius:10px; margin:0px 0px 0px 0px")
+        self.setFixedHeight(200)
+        self.setGraphicsEffect(Shadow())
+        #--- Titulo----
+        labelTitle = QLabel("Visualización")
+        labelTitle.setStyleSheet("""background: #0d47a1;
+                                    color: white;
+                                    font: 20px;
+                                    font-weight:bold;
+                                    border-top-left-radius: 10px;
+                                    border-top-right-radius: 10px;
+                                    border-bottom-right-radius: 0px;
+                                    border-bottom-left-radius: 0px;
+                                    margin:0px 0px 0px 0px""")
+        labelTitle.setAlignment(Qt.AlignCenter)
+
+        labelSelecionar = QLabel("Selecionar:")
+        labelSelecionar.setStyleSheet("background:rgba(0,0,0,0);color:black;margin: 30px 0px 0px 0px")
+        labelSelecionar.setAlignment(Qt.AlignCenter)
+
+        #---Lista--
+        self.listaVariables = QComboBox()
+        self.listaVariables.setStyleSheet("background:#5472d3;color:black; font:18px; padding: 5px 0px 5px 10px; selection-background-color: #0d47a1; margin: 0px 0px 0px 10px; border-radius:10px")
+        self.sensores = ["Temperatura A","Humedad A","Temperatura 1","Humedad 1",
+                        "Temperatura 2", "Humedad 2", "Temperatura 3", "Humedad 3",
+                        "Temperatura 4", "Humedad 4", "Temperatura 5", "Humedad 5", "Humedad grano"]
+        self.listaVariables.addItems(self.sensores)
+
+        box = QLabel("")
+        box.setStyleSheet("background: transparent;margin: 20px")
+
+        vbox = QVBoxLayout(self)
+        vbox.addWidget(labelTitle)
+        vbox.addWidget(labelSelecionar)
+        vbox.addWidget(self.listaVariables)
+        vbox.addWidget(box)
+        vbox.setContentsMargins(0,0,0,0)
+class MenuBar:
+    def __init__(self,menuBar):
+        self.menuBar = menuBar
+        self.menuBar.setStyleSheet("background: #002171; color:white") #263238
+
+        # Estado de la aplicacion
+        self.state = QLabel()
+        self.state.setStyleSheet('font: 13px; font-weight:bold')
+        hbox = QHBoxLayout(self.menuBar)
+        hbox.addWidget(self.state)
+        hbox.setAlignment(Qt.AlignCenter)
+
+        archivoMenu = self.menuBar.addMenu('Archivo')
+        preferenciasAction = QAction('Preferencias',self.menuBar, triggered=self.showDialog)
+        archivoMenu.addAction(preferenciasAction)
+    def showDialog(self):
+        self.loginWindow = login.Login()
+
+class Graph(QFrame):
+    def __init__(self):
+        super().__init__()
+        self.setStyleSheet("background-color:white;border-radius:10px")
+        self.setGraphicsEffect(Shadow())
+        self.figure = FigureCanvas(Figure(figsize=(2, 12),dpi = 100))
+        self.figure.setStyleSheet("background-color: rgba(60,60,60,150)")
+
+        self.ax1 = self.figure.figure.add_subplot(111)
+        self.formato = dates.DateFormatter("%H:%M")
+
+        vboxFigura = QVBoxLayout(self)
+        vboxFigura.addWidget(self.figure)
+
+class Loading(QtWaitingSpinner):
+    def __init__(self, parent, ):
+        super().__init__(parent)
+        self.setRoundness(70.0)
+        self.setMinimumTrailOpacity(15.0)
+        self.setTrailFadePercentage(70.0)
+        self.setNumberOfLines(12)
+        self.setLineLength(20)
+        self.setLineWidth(5)
+        self.setInnerRadius(20)
+        self.setRevolutionsPerSecond(1)
+        self.setColor(QColor(81, 4, 71))
